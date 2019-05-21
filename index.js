1a826d50b03c8299b64d9ecdbdf8eb1c7dca4a1c
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-05-21 19:25:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ function create() {
   this.physics.add.collider(real.hero, GroundBlackOne)
   this.physics.add.collider(real.hero, SkyOne)
   GroundBlackOne.setCollisionByProperty({collides:true}) 
+  GroundBlackOne.setCollision({20,21,22,23,24,25})
   SkyOne.setCollisionByProperty({collides:true}) 
 }
 function update() {
