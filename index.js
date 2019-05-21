1fbd08cc50a746f0bc06d9c22adff47a8092ff81
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-05-21 17:01:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-
+var config = {
+  type: Phaser.AUTO,
+  width: 850,
+  height: 600,
+  backgroundColor: "b9eaff",
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      enableBody: true,
+    }
+  },
+  scene: {
+    preload,
+    create,
+    update
+  }
+}
+var game = new Phaser.Game(config)
+function preload() {
+  this.load.image('dvdtileone','assets/dvdtileone.png')
+  this.load.tilemapTiledJSON('dvdmapone','assets/dvdmapone.json')
+}
