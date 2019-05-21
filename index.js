1f366e30e51767fe3e91d52aac0ccdfb9f7486b0
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-05-21 19:03:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -16,26 +16,28 @@ var config = {
     update
   }
 }
+const real = {}
 var game = new Phaser.Game(config)
 function preload() {
   this.load.image('dvdtileone','assets/dvdsetone.png')
   this.load.tilemapTiledJSON('dvdmapone','assets/dvdmapone.json')
 }
 function create() {
-  var hero = this.load.image(10, 5 , 'hero')
+  var real.hero = this.load.image(10, 5 , 'hero')
   var map = this.add.tilemap('dvdmapone')
   var dvdtileone = map.addTilesetImage('tilesetone','dvdtileone')
   var SkyOne = map.createStaticLayer("SkyOne", [dvdtileone], 0, 0)
   var GroundBlackOne = map.createStaticLayer("GroundBlackOne", [dvdtileone], 0, 0)
-  var keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-  var keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-  var keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-  this.physics.add.collider(hero, GroundBlackOne)
-  this.physics.add.collider(hero, SkyOne)
+  var real.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+  var real.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+  var real.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+  this.physics.add.collider(real.hero, GroundBlackOne)
+  this.physics.add.collider(real.hero, SkyOne)
   GroundBlackOne.setCollisionByProperty({collides:true}) 
   SkyOne.setCollisionByProperty({collides:true}) 
 }
 function update() {
-  if(keyD.isDown) {
+  if(real.keyD.isDown) {
+    real.hero.x += 5
   }
 }
