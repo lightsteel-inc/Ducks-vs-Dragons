0fdd46ba1d66d292b8b3edcd754ffa8bb1efc00b
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-05-22 20:49:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ this.physics.add.collider(real.hero, SkyOne)
 this.anims.create
 ({
     key: 'left',
-    frames: this.anims.generateFrameNumbers('real.hero', { start: 0, end: 3 }),
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
     frameRate: 10,
     repeat: -1
 });
@@ -54,14 +54,14 @@ this.anims.create
 this.anims.create
 ({
     key: 'turn',
-    frames: [ { key: 'real.hero', frame: 4 } ],
+    frames: [ { key: 'dude', frame: 4 } ],
     frameRate: 20
 });
 
 this.anims.create
 ({
     key: 'right',
-    frames: this.anims.generateFrameNumbers('real.hero', { start: 5, end: 8 }),
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
     frameRate: 10,
     repeat: -1
 });
