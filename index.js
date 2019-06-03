2fce4d53d5e3c80385f103c66bff2b56ce775afe
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-06-03 17:59:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var canvas = document.getElementById('canvas')
 var ctx = canvas.getContext('2d')
 
 function rect(x,y,width,height,color){
-  ctx.beginPath()
-  ctx.fillStyle = color
-  ctx.rect(x,y,width,height)
-  ctx.fill()
-  ctx.closePath()
+  this.x = x
+  this.y = y
+  this.width = width
+  this.height = height
+  this.color = color
 }
 var background = new rect(0,0,canvas.width,canvas.height,'#B5D8F6')
