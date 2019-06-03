9a56d2dcfbbc8d30d906cd2e8225a12ad7548487
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-06-03 17:22:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById('canvas')
 var ctx = canvas.getContext('2d')
 
-function rect(x,y,width,height,){
+function rect(x,y,width,height,color){
   ctx.beginPath()
   ctx.fillStyle = 'color'
   ctx.rect(x,y,width,height)
