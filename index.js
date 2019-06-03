6346367c1e9b07663b00d3dfb570b19c4d2dd036
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-06-03 17:11:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 var canvas = document.getElementById('canvas')
 var ctx = canvas.getContext('2d')
+
+function rect(x,y,width,height,){
+  context.beginPath()
+  context.fillStyle = 'color'
+  context.rect(x,y,width,height)
+  context.fill()
+  context.closePath()
+}
