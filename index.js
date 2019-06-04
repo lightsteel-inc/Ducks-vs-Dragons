eea3c1ae8bf14697743235e57d414cf4715ba7d6
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-06-04 00:20:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 var canvas = document.getElementById('canvas')
 var context = canvas.getContext('2d')
 
-function rect(x,y,width,height ,color){
-  context.beginPath()
-  context.fillStyle = color
-  context.rect(x,y,width,height)
-  context.fill()
-  context.closePath()
-}
-    var BackGround =  rect(0,0,canvas.width,canvas.height,'#B5D8F6')
-    var ObOne = rect(450,325,100,10,'#000000')
-    var ObTwo = rect(100,325,100,10,'#000000')
-    var ObThree = rect(225,505,200,10,'#000000')
+context.beginPath()
+context.fillStyle = '09DEFF'
+context.rect(0,0,canvas.width,canvas.height)
+context.fill()
 
 
 
