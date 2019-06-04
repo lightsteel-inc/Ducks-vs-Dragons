c9ca782f4bd88dcdde3fd917c55a92b0b88f46dd
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-06-04 00:21:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById('canvas')
 var context = canvas.getContext('2d')
 
 context.beginPath()
-context.fillStyle = '09DEFF'
+context.fillStyle = '#09DEFF'
 context.rect(0,0,canvas.width,canvas.height)
 context.fill()
 
