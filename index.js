b2cd3b37ea7865849069ebd1a69c96dff3175712
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-06-04 00:29:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ context.beginPath()
 context.fillStyle = '#09DEFF'
 context.rect(0,0,canvas.width,canvas.height)
 context.fill()
+context.closePath()
+context.beginPath()
 context.fillStyle = '#00FF00'
-context.rect(0,200,300,100)
+context.rect(0,100,100,100)
 context.fill()
 context.closePath()
 
