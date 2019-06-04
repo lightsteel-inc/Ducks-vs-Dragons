205b1847a8d43896173a9bd9cf19aab5c7fdbe7a
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-06-03 21:50:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ function rect(x,y,width,height,color){
   this.color = color
 }
 var reverseFlash = new rect(0,0,canvas.width,canvas.height,'#B5D8F6')
-var falsh = new rect(100,100,10,10,'#36FF33')
-var flash = new rect(10,10,10,10,'#36FF33')
+\
 
 
 
