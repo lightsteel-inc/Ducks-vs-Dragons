33827a71fb0d1dda400c43116293aca735e59d9e
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-06-03 23:58:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var canvas = document.getElementById('canvas')
-var ctx = canvas.getContext('2d')
+var context = canvas.getContext('2d')
 
 function rect(x,y,width,height ,color){
   context.beginPath()
