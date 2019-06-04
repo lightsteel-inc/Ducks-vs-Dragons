07fa8d2d2b338cbc01716eb6a6e098ad39554b9f
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-06-03 21:54:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var canvas = document.getElementById('canvas')
 var ctx = canvas.getContext('2d')
 
-function rect(x,y,width,height,color){
-  this.x = x
+function rect(x,y,width,height ,color){
+  this.x = x 
   this.y = y
   this.width = width
   this.height = height
   this.color = color
 }
-var reverseFlash = new rect(0,0,canvas.width,canvas.height,'#B5D8F6')
+var  = new rect(0,0,canvas.width,canvas.height,'#B5D8F6')
 
 
 
